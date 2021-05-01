efbc346517f8f144b5110cb4e5940ff7bd599fef
Jon Gregory <jongregory75@hotmail.com>
2021-04-30 23:31:18 -0600

    added hour color blocking

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,23 @@
+//Generates Date for current day in header
+
 $(document).ready(function () {
-  //Generates Date for current day in header
   $("#currentDay").text(moment().format("dddd, MMMM Do  YYYY"));
+
+  function checkTime() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      //check the currentHour vs blockHour
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour == currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
 
   //event handler for save button
   $(".saveBtn").on("click", function () {
@@ -10,25 +27,14 @@ $(document).ready(function () {
     localStorage.setItem(time, value);
   });
 
-  //build out local storage for each time block
-  $("#hour-9 .description").val(localStorage.getItem("hour-9", value));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10", value));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11", value));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12", value));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13", value));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14", value));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15", value));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16", value));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17", value));
+  //   //build out local storage for each time block
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
-
-$("#btn").on("click", function () {
-  var id = $(this).aattr("data-id");
-  var text = $("#" + id).val();
-  localStorage.setItem(id, text);
-});
-
-var currentHour = moment().hour();
-console.log(currentHour);
-
-var time = [9, 10, 11, 12, 1, 2, 3, 4, 5];
